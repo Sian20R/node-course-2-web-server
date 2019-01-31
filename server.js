d6fde64b14464f3b2c2cb8686821d70af6ffdb6a
Seaw Ker Boon <seawkerboon@gmail.com>
2019-01-31 15:30:09 +0800

    Setup start script and Heroku port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -61,6 +62,9 @@ app.get('/bad', (req, res) => {
 })
 
 // To bind the port
-app.listen(3000, () => {
-    console.log('Server is up on port 3000');
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
 })
+
+
+///Users/admin/Desktop/OneDrive - Singapore Institute Of Technology/Practice/Nodejs/Udemy/Tutorials/Section_5/node-web-server
